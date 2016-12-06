241e67b1e914ecfde6474a937e3afad07eb75da4
Steve Chronister <stephen.g.chronister@gmail.com>
2016-12-06 11:53:44 -0600

    updates to frontend

--- frontend.js
+++ frontend.js
@@ -1,7 +1,24 @@
+var imports = require("./server.js");
+
+console.log(imports.table);
+
+
 $("#viewTables").on("click",function(){
 	$.get("/tables", function(data){
 		console.log(data);
-	});
+	}).done(function(){
+		for (var i = 0; i < imports.table.length; i++){
+			//show the divs for each table reservation
+			$("#tableArea").append("<div id = '" +imports.table[i]+"'></div>");
+			$("#"+imports.table[i]).append("<h2>"+(i+1)+" | " + imports.table[i].customerID);
+		}
+		for (var i = 0; i < imports.waitlist.length; i++){
+			//show the divs for each table reservation
+			$("#waitlistArea").append("<div id = '" +imports.waitlist[i]+"'></div>");
+			$("#"+imports.waitlist[i]).append("<h2>"+(i+1)+" | " + imports.waitlist[i].customerID);
+		}
+	}
+	)
 });
 
  	
