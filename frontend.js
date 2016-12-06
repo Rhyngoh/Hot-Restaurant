404051f75036f7f7190eea40ad064902a19fd470
Steve Chronister <stephen.g.chronister@gmail.com>
2016-12-06 11:56:29 -0600

    fix to frontend

--- frontend.js
+++ frontend.js
@@ -6,13 +6,13 @@ $("#viewTables").on("click",function(){
 	}).done(function(){
 		for (var i = 0; i < imports.table.length; i++){
 			//show the divs for each table reservation
-			$("#tableArea").append("<div id = '" +imports.table[i]+"'></div>");
-			$("#"+imports.table[i]).append("<h2>"+(i+1)+" | " + imports.table[i].customerID);
+			$("#tableArea").append("<div id = 'table" +imports.table[i]+"'></div>");
+			$("#table"+imports.table[i]).append("<h2>"+(i+1)+" | " + imports.table[i].customerID);
 		}
 		for (var i = 0; i < imports.waitlist.length; i++){
 			//show the divs for each table reservation
-			$("#waitlistArea").append("<div id = '" +imports.waitlist[i]+"'></div>");
-			$("#"+imports.waitlist[i]).append("<h2>"+(i+1)+" | " + imports.waitlist[i].customerID);
+			$("#waitlistArea").append("<div id = 'wait" +imports.waitlist[i]+"'></div>");
+			$("#wait"+imports.waitlist[i]).append("<h2>"+(i+1)+" | " + imports.waitlist[i].customerID);
 		}
 	}
 	)
